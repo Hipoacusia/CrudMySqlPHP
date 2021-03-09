dec7f61367c69c3d77897211843a0ff60cfdd01b
Hipoacusia <musicapbc2@gmail.com>
2021-03-08 21:38:30 -0500

    PHP CRUD MySql

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  '25798552Jose',
+  'phpmycrud'
+);
+
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Crud php Mysql</title>
+  <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container">
+	 <a href="index.php" class="navbar-brand">PHP Mysql Crud</a>
+  </div>
+</nav>
